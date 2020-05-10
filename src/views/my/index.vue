82a5b270939b785b748c0db258a5fbbe0a56d74d
xiuxiu <2457982187@qq.com>
2020-05-10 21:44:53 +0800

    '我的布局未完成'

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,6 +1,20 @@
 <template>
 <div class="my-container">
-我的
+<van-cell-group>
+  <van-cell>
+    <van-image
+      round
+      width="50px"
+      height="50px"
+      fit="cover"
+      src="https://img.yzcdn.cn/vant/cat.jpeg"
+    />
+    <template #title>
+      <span class="custom-title">单元格</span>
+      <van-tag type="danger">标签</van-tag>
+    </template>
+  </van-cell>
+</van-cell-group>
 </div>
 </template>
 
