eda14ac44d465c8fd3ab88c2224b0ae6fea8a48e
xiuxiu <2457982187@qq.com>
2020-05-13 12:19:37 +0800

    '顶部导航'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
 <div class="home-container">
-首页
+<van-cell value="内容" class="nav-top">
+  <template #default>
+    <div class="logo"></div>
+    <van-button type="default" round class="search-btn" icon="search">搜索</van-button>
+  </template>
+</van-cell>
 </div>
 </template>
 
@@ -22,5 +27,27 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.home-container {
+  .van-cell {
+    background-color: #3196fa;
+    .van-cell__value {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .logo {
+      width: 95px;
+      background: url(./logo.png) no-repeat;
+      background-size: cover;
+    }
+    .search-btn {
+      background-color: rgba(250, 240, 240, 0.3);
+      color: #fff;
+      width: 147px;
+      height: 26px;
+      border: unset;
+    }
+  }
+}
 
 </style>
